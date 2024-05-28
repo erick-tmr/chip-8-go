c4b331f3ed9dab7dca07f5562516da2fa0aafd52
Erick Takeshi <erick.rezende@iugu.com>
2024-05-28 11:44:44 -0300

    add stack to cpu

--- pkg/cpu/cpu.go
+++ pkg/cpu/cpu.go
@@ -1,9 +1,14 @@
 package cpu
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/erick-tmr/chip-8-go/internal/data_structures"
+)
 
 var program_counter uint16
 var i_register uint16
+var call_stack data_structures.Stack32[uint16]
 
 func Start() {
 	fmt.Println("CPU initialized")
