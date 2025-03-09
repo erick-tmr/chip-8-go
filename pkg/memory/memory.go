3bf854535dbbc69c04b2b9e1fa22daab585898a7
Erick Takeshi <erick.tmr@outlook.com>
2025-03-09 00:38:40 -0300

    read and write to memory

--- pkg/memory/memory.go
+++ pkg/memory/memory.go
@@ -1,18 +1,46 @@
 package memory
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
+
+const LastProtectedAddress = 0x1FF
+const FinalAddress = 0xFFF
 
 type Memory struct {
-	Space   [4096]byte
-	private string
+	space [4096]byte
 }
 
 func New() Memory {
-	fmt.Println("Memory initialized")
-	mem_instance := Memory{
-		[4096]byte{},
-		"Im private",
+	return Memory{}
+}
+
+func (m *Memory) Load(b byte, addr uint16) error {
+	if addr <= LastProtectedAddress {
+		return errors.New("memory: trying to insert into protected space")
 	}
 
-	return mem_instance
+	if addr > FinalAddress {
+		return errors.New("memory: addr is too large for memory")
+	}
+
+	m.space[addr] = b
+
+	return nil
+}
+
+func (m *Memory) Copy(source []byte, startAddr uint16) error {
+	if len(source) > (FinalAddress - LastProtectedAddress) {
+		return errors.New("memory: memory overflow, trying to copy too big source")
+	}
+
+	for i, byteValue := range source {
+		err := m.Load(byteValue, startAddr+uint16(i))
+		if err != nil {
+			return fmt.Errorf("memory/copy: error loading byte / %v", err)
+		}
+	}
+
+	return nil
 }
